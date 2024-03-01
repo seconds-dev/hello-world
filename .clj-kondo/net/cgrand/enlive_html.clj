4dbfe1098a9472c835c96b54ee9eb774c1286b28
洪盟翔 <andy@seconds.dev>
2024-03-01 16:30:54 +0800

    Update enlive_html.clj

--- .clj-kondo/net/cgrand/enlive_html.clj
+++ .clj-kondo/net/cgrand/enlive_html.clj
@@ -3,9 +3,9 @@
 (defmacro deftemplate
  "Defines a template as a function that returns a seq of strings."
  [name _source args & forms]
-  `(def ~name (fn [~@args] ~@forms)))
+  `(def ~name [(fn [~@args] ~@forms)]))
 
 (defmacro defsnippet
  "Define a named snippet -- equivalent to (def name (snippet source selector args ...))."
  [name _source _selector args & forms]
- `(def ~name (fn  [~@args] ~@forms)))
+ `(def ~name [(fn [~@args] ~@forms)]))
